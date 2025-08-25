e1db71fe8663128d3970734fe8ad52e6d32b3f71
Bharat Kumar <bharat.nanavathula@gmail.com>
2025-08-25 14:23:19 +0530

    a

--- app/dashboard/subscription-buy/cancel/page.tsx
+++ app/dashboard/subscription-buy/cancel/page.tsx
@@ -1,5 +1,11 @@
-import PaymentFailed from '../../../../app/components/subscription/list/subscription-cancel'
+"use client"
 import React from 'react'
+import dynamic from "next/dynamic";
+
+const PaymentFailed = dynamic(
+  () => import("../../../../app/components/subscription/list/subscription-cancel"), 
+  { ssr: false }
+);
 
 export default function Page() {
   return (
